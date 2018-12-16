fdcd49d831e44a9120689968c4ff56c9d96c5f0f
Pierre Kisters <pierrekisters@gmail.com>
2018-12-16 20:50:52 +0100

    write PGM files

--- aufgabe7.cpp
+++ aufgabe7.cpp
@@ -22,6 +22,9 @@
 
 using namespace std;
 
+/**
+ * Struct for the header of the BMP file format
+ */
 #pragma pack(1)
 struct bmpHeader {
     uint16_t bfType;
@@ -41,7 +44,22 @@ struct bmpHeader {
     uint32_t biClrImportant;
 };
 
-void writeBmp(ofstream &ofs, const char *grayBytes, int width, int height) {
+/**
+ * Write the image as BMP
+ *
+ * @param filename name of the output file
+ * @param colors image as flat array of graytones
+ * @param width width of the image
+ * @param height height of the image
+ */
+void writeBmp(string filename, unsigned char *colors, int width, int height) {
+    ofstream ofs; // Dateistream initialisieren
+    ofs.open(filename, ofstream::binary); // Datei öffnen
+    if (!ofs) { // Prüfung ob Datei geöffnet werden konnte
+        cerr << endl << "ERROR: Datei konnte nicht geöffnet werden" << endl;
+        exit(2);
+    }
+
     const int bitmapFileHeaderLength = 14;
     const int bitmapInfoHeaderLength = 40;
     const int bitmapHeaderLength = bitmapFileHeaderLength + bitmapInfoHeaderLength;
@@ -66,29 +84,30 @@ void writeBmp(ofstream &ofs, const char *grayBytes, int width, int height) {
 
     ofs.write((char *) &header, sizeof(header));
 
-    char bytes[width * height * 4];
-
     int i = 0;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            char color = *(grayBytes + (height - y - 1) + x * height);
-            bytes[i++] = color;
-            bytes[i++] = color;
-            bytes[i++] = color;
-            bytes[i++] = 0;
+            const int invertedY = height - y - 1;
+            char color = colors[x * height + invertedY];
+            char bytes[] = {color, color, color, 0};
+            ofs.write(bytes, sizeof(bytes));
         }
     }
 
-    ofs.write(bytes, sizeof(bytes));
+    ofs.close();
 }
 
 /**
+ * Read an image from a PGM file
  *
+ * @param filename name of the file
+ * @param width returned width of the image
+ * @param height returned height of the image
+ * @return colors image as flat array of graytones
  */
-void aufgabe_7_1() {
-    string ifs_filename = "../dreifach-7-1.pgm"; // Dateiname
+unsigned char *readPgm(string filename, int &width, int &height) {
     ifstream ifs; // Dateistream initialisieren
-    ifs.open(ifs_filename); // Datei öffnen
+    ifs.open(filename); // Datei öffnen
     if (!ifs) { // Prüfung ob Datei geöffnet werden konnte
         cerr << endl << "ERROR: Datei konnte nicht geöffnet werden" << endl;
         exit(1);
@@ -96,41 +115,80 @@ void aufgabe_7_1() {
 
     string field;
     ifs >> field;
+
+    if (strcmp(field.c_str(), "P2") != 0) {
+        cerr << endl << "ERROR: Falsches Format" << endl;
+        exit(1);
+    }
+
     ifs >> field;
-    const int width = atoi(field.c_str());
+    width = stoi(field);
     ifs >> field;
-    const int height = atoi(field.c_str());
+    height = stoi(field);
     ifs >> field;
 
-    char colors[width][height];
+    auto *colors = (unsigned char *) malloc(width * height * sizeof(unsigned char));
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             ifs >> field;
-            colors[x][y] = (char) atoi(field.c_str());
+            colors[x * height + y] = (unsigned char) stoi(field);
         }
     }
 
     ifs.close();
 
-    string ofs_filename = "../dreifach-out-7-1.bmp"; // Dateiname
+    return colors;
+}
+
+
+/**
+ * Write the image as PGM
+ *
+ * @param filename name of the output file
+ * @param colors image as flat array of graytones
+ * @param width width of the image
+ * @param height height of the image
+ */
+void writePgm(string filename, unsigned char *colors, int width, int height) {
     ofstream ofs; // Dateistream initialisieren
-    ofs.open(ofs_filename, ofstream::binary); // Datei öffnen
+    ofs.open(filename); // Datei öffnen
     if (!ofs) { // Prüfung ob Datei geöffnet werden konnte
         cerr << endl << "ERROR: Datei konnte nicht geöffnet werden" << endl;
         exit(2);
     }
 
-    writeBmp(ofs, (char *) &colors, width, height);
+    ofs << "P2" << endl;
+    ofs << width << " " << height << endl;
+    ofs << 255 << endl;
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            unsigned char color = colors[x * height + y];
+            ofs << " " << setw(3) << (int) color;
+        }
+        ofs << endl;
+    }
 
     ofs.close();
+}
 
-    /*for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            ifs >> field;
-            colors[x][y] = (char) atoi(field);
-        }
-    }*/
+/**
+ * Aufgabe 7.1
+ * Read and then write an image
+ */
+void aufgabe_7_1() {
+    string ifs_filename = "../dreifach-7-1.pgm"; // Dateiname
+    int width, height;
+    unsigned char *colors = readPgm(ifs_filename, width, height);
+
+    string ofs_filename = "../dreifach-out-7-1.bmp"; // Dateiname
+    writeBmp(ofs_filename, colors, width, height);
+
+    string ofs_filename2 = "../dreifach-out-7-1.pgm"; // Dateiname
+    writePgm(ofs_filename2, colors, width, height);
+
+    free(colors);
 }
 
 //-----------------------------------------------------------------------------
